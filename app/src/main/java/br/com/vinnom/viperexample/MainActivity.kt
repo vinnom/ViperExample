48e85ce7edab2047e1e74d9e17ad98645742d153
vinnom <vinnom.2d@gmail.com>
2021-12-14 00:01:08 -0300

    add view and configure

--- app/src/main/java/br/com/vinnom/viperexample/MainActivity.kt
+++ app/src/main/java/br/com/vinnom/viperexample/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.com.vinnom.viperexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.vinnom.viperexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
