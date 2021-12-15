abaec4cdad1a221127e596c1069a421d3365b09d
vinnom <vinnom.2d@gmail.com>
2021-12-14 23:20:13 -0300

    define GetRandomReferenceFragmentView as the first screen

--- app/src/main/java/br/com/vinnom/viperexample/MainActivity.kt
+++ app/src/main/java/br/com/vinnom/viperexample/MainActivity.kt
@@ -3,11 +3,21 @@ package br.com.vinnom.viperexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.vinnom.viperexample.databinding.ActivityMainBinding
+import br.com.vinnom.viperexample.view.GetRandomReferenceFragmentView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.apply {
+                setReorderingAllowed(true)
+                add(R.id.fragment_container_view, GetRandomReferenceFragmentView())
+            }
+            transaction.commit()
+        }
     }
 }
